647879fab08406982258430fa607765c1a52a2df
Ceandf16 <carlos97cc123@hotmail.com>
2024-03-12 16:21:11 -0500

    Diseño completado

--- src/AgregarDatos.jsx
+++ src/AgregarDatos.jsx
@@ -1,3 +1,4 @@
+
 export function AgregarDatos() {
   return (
     <div>
--- src/ListaTareas.jsx
+++ src/ListaTareas.jsx
@@ -1,3 +1,24 @@
 export function Lista() {
-  return <p1>Aqui esta lista</p1>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Titulo</th>
+          <th>Descripción</th>
+          <th>Estado</th>
+          <th>Fecha Inicio</th>
+          <th>Fecha Fin</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Limpiar casa</td>
+          <td>Barrer</td>
+          <td>Incompleto</td>
+          <td>12/03/2024</td>
+          <td>13/03/2024</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
